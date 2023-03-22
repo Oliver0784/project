8c199e03a60ed173171d8b21c3e759e74a1d1b1f
Oliver0784 <oliveradede08@gmail.com>
2023-03-22 11:21:43 +0300

    signUp page and Login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
-import Register from './pages/Register'
 import Login from './pages/Login'
 import { Outlet } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import {Routes, Route } from "react-router-dom";
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<LandingPage/>} />
         <Route path="/about" element={<LandingPage />} />
         <Route path="/contact" element={<LandingPage/>} />
-        <Route path="/register" element={<Register/>} />
+        <Route path="/register" element={<SignUp/>} />
         <Route path="/login" element={<Login/>} />
 
       </Routes>
